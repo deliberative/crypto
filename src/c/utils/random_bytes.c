bb255a5a857856c5cd4e9e94c96af625fcf45806
fuzzc0re <dim@deliberative.io>
2023-10-27 17:11:28 +0300

    Fixed problems with randombytes

--- src/c/utils/random_bytes.c
+++ src/c/utils/random_bytes.c
@@ -22,10 +22,32 @@ uint32_t
 randombytes_random(void)
 {
   return EM_ASM_INT_V({
-    const crypto = globalThis.crypto;
-    const buf = new Uint32Array(1);
-    crypto.getRandomValues(buf);
-    return buf[0] >>> 0;
+    if (Module.getRandomValue === undefined) {
+        try {
+            var window_ = 'object' === typeof window ? window : self;
+            var crypto_ = typeof window_.crypto !== 'undefined' ? window_.crypto : window_.msCrypto;
+            var randomValuesStandard = function() {
+                var buf = new Uint32Array(1);
+                crypto_.getRandomValues(buf);
+                return buf[0] >>> 0;
+            };
+            randomValuesStandard();
+            Module.getRandomValue = randomValuesStandard;
+        } catch (e) {
+            try {
+                var crypto = require('crypto');
+                var randomValueNodeJS = function() {
+                    var buf = crypto['randomBytes'](4);
+                    return (buf[0] << 24 | buf[1] << 16 | buf[2] << 8 | buf[3]) >>> 0;
+                };
+                randomValueNodeJS();
+                Module.getRandomValue = randomValueNodeJS;
+            } catch (e) {
+                throw 'No secure random number generator found';
+            }
+        }
+    }
+    return Module.getRandomValue();
   });
 }
 
